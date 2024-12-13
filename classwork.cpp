56f49b196e47638550698ed138bdb2f1b7e277aa
Guminc1 <zombiekillerranger@gmail.com>
2024-12-13 23:31:04 +0000

    Passowrd

--- classwork.cpp
+++ classwork.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 #include <string>
-#include <thread>
-#include <chrono>
 
 void password(std::string& Answer) {
     if (Answer == "Skibidi") {
         std::cout << "Hello Master" << "\n";
     } else {
-        std::cout << "Wrong" << "\n";
-        std::this_thread::sleep_for(std::chrono::seconds(1)); 
-        std::cout << "Try again" << "\n"; 
+        std::cout << "Wrong, try again" << "\n";
         std::cin >> Answer; 
         password(Answer);
     }
