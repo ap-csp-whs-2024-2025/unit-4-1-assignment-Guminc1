341fb2fd2887578144abf0901a348649ac2e90e6
Guminc1 <zombiekillerranger@gmail.com>
2024-12-13 22:15:43 +0000

    sigma mode

--- classwork.cpp
+++ classwork.cpp
@@ -6,12 +6,19 @@ int main()
   int age = 10;
   double price = 19.99;
   bool hasPets =true;
+  std::string Skibidi;
 
   std::string teacher = "Mr. Noob";
   
   std::cout << "I am " << age << " years old." << "\n";
 
   std::cout << "My teacher name is " << teacher << "\n";
+
+  std::cout << "What is the password?" << "\n";
+
+  std::cin >> Skibidi;
+
+  std::cout << "Hello Master " << Skibidi << " Welcome" << "\n"; 
 
   return 0;
 }
