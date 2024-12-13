054e3264f7185ecab42f5df03ae90bee1dc89fd9
Guminc1 <zombiekillerranger@gmail.com>
2024-12-13 23:28:56 +0000

    New(Password)

--- classwork.cpp
+++ classwork.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
 
-int main()
-{
-  std::string Answer; 
-  std::cout << "What is the password?" << "\n";
-  std::cin >> Answer;
+void password(std::string& Answer) {
+    if (Answer == "Skibidi") {
+        std::cout << "Hello Master" << "\n";
+    } else {
+        std::cout << "Wrong" << "\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1)); 
+        std::cout << "Try again" << "\n"; 
+        std::cin >> Answer; 
+        password(Answer);
+    }
+}
 
-  if (Answer == "Skibidi") {
-    std::cout << "Hello Master" << "\n";
-  } else {
-    std::cout << "Wrong" << "\n";
-  }
+int main() {
+    std::string Answer;
+    std::cout << "What is the password?" << "\n";
+    std::cin >> Answer;
 
-  return 0;
+    password(Answer);
+
+    return 0;
 }
